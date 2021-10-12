f13a79c67c8eb3c2ed64bccb9a1ccc5677e9b3f7
jahenner <jahenner@gmail.com>
2021-10-12 12:31:46 -0400

    Finished Project minus app icon working

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "u2oFgZDDkLQKN71d0dqdTdxzV", consumerSecret: "V9HQpYW5VAS76vzcGO3ca7LmQuOybS7VgQTeMbPqxz1yfFzgyX")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
